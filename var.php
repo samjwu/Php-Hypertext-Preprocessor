3431c4448b09b80bdb651a1f4e485859e152a5e8
Sam Wu <sjwu@ualberta.ca>
2017-07-18 22:06:34 -0700

    output var

---
+++ var.php
@@ -0,0 +1,17 @@
+<?php
+
+#outputting variables
+$var = "variable";
+$num1 = 1;
+$num2 = 2;
+
+//method 1
+echo "This is a $var.";
+
+//method 2
+echo "This is also a " . $var . ".";
+
+//summation of variables
+echo $num1 + $num2;
+
+?>
