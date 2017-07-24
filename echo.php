60053d0e2e93665b1962a4c730c4993f30f8a7a7
Sam Wu <sjwu@ualberta.ca>
2017-07-23 21:55:46 -0700

    output with echo

---
+++ echo.php
@@ -0,0 +1,15 @@
+<?php
+
+#echo
+
+/*
+echo outputs data to the screen
+echo has no return value, can take multiple parameters, and is faster than print
+*/
+$text = "text";
+$num1 = 1;
+$num2 = 2;
+echo "This ", "string ", "has ", "5 ", "parameters/arguments.";
+echo "echo can also output $text and variables"
+echo $num1 + $num2; //echo can output sums
+?>
