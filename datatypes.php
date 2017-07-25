89ab7bd9c41c8c48c9dbef632b657d7a5fccbc34
Sam Wu <sjwu@ualberta.ca>
2017-07-24 22:51:51 -0700

    data types

---
+++ datatypes.php
@@ -0,0 +1,18 @@
+<?php
+
+
+#Data Types
+
+/*
+Strings are sequences of characters
+Use single or double quotes ('' or "")
+*/
+$string = "This is a string";
+
+/*
+Integers are numbers between -(2^31) and 2^31 - 1
+The three formats are decimal, hexadecimal (0x) and octal (0)
+*/
+$integer = 10;
+
+?>
