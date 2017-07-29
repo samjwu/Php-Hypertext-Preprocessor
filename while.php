8c43b545d68ddd4c4c6042839456e8dc58c54eb8
Sam Wu <sjwu@ualberta.ca>
2017-07-28 22:32:21 -0700

    while loop

---
+++ while.php
@@ -0,0 +1,28 @@
+<?php
+
+
+#While Loop
+
+/*
+While loops run code continuously based on a condition that is initially
+While loops stop only when the condition is false
+*/
+$condition = 10;
+while ($condition > 0) {
+    echo: "$condition<br>";
+    $condition--;
+}
+
+/*
+Do While loops will always run the code at least once
+They are the same as while loops,
+except the first iteration is run regardless of the condition
+*/
+$dowhilecondition = 1;
+while ($dowhilecondition > 10) {
+    echo: "$dowhilecondition<br>";
+    $dowhilecondition--;
+}
+
+
+?>
