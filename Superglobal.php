b8d10d03220bb42b350439edb61f2d3ea1d54e7b
Sam Wu <sjwu@ualberta.ca>
2017-07-29 21:36:05 -0700

    $_SERVER superglobal

--- Superglobal.php
+++ Superglobal.php
@@ -19,11 +19,19 @@ $_SESSION
 */
 
 /*
-$GLOBALS is a global array that contains global variables anywhere in the PHP script
+$GLOBALS is an array that contains global variables anywhere in the PHP script
 Its indices are the names of the global variables
 */
 $globvar = 10;
 echo $GLOBALS["globvar"];
+
+/*
+$_SERVER is an array with information about headers, paths, and script locations
+*/
+echo $_SERVER['PHP_SELF'] . "<br>"; //return the name of this PHP script
+//http://www.webopedia.com/TERM/H/hosting_server.html
+echo $_SERVER['SERVER_NAME'] . "<br>"; //return name of the host server
+echo $_SERVER['HTTP_REFERER'] . "<br>"; //return URL of page on which PHP script runs
 
 
 ?>
