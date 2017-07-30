2467c1efa7cd20fe29c02dc8f6072b10883c2904
Sam Wu <sjwu@ualberta.ca>
2017-07-29 20:57:40 -0700

    ex fxn

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php
+
+
+#Functions
+
+/*
+PHP function names are not case sensitive
+Default parameters/arguments can be set for functions
+*/
+function fxn($defaultarg = 10) {
+    echo "The default value is 10<br>";
+    echo "The value was set to $defaultarg<br>"
+    return $defaultarg;
+}
+
+
+?>
