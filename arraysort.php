de3e999e83fa6df4b37ad1433ac80a6ca2884b04
Sam Wu <sjwu@ualberta.ca>
2017-07-29 21:24:43 -0700

    sort by key

--- arraysort.php
+++ arraysort.php
@@ -6,17 +6,31 @@
 /*
 PHP has several built-in functions for sorting arrays:
 */
+$words = array("Art", "Book", "Calligraphy");
+$numbers = array(1, 2, 3);
+$wordrank = array("Art" => 1, "Book" => 2, "Calligraphy" => 3);
 
 /*
 The sort() function sorts indexed arrays in ascending order
 The rsort() function sorts indexed arrays in descending order
 */
-$words = array("Art", "Book", "Calligraphy");
-$numbers = array(1, 2, 3);
 sort($words);
 sort($numbers);
 rsort($words);
 rsort($numbers);
+
+/*
+The ksort() function sorts associative arrays in ascending order by key
+The krsort() function sorts associative arrays in descending order by key
+*/
+ksort($wordrank);
+krsort($wordrank);
+
+/*
+The asort() function sorts associative arrays in ascending order by value
+The arsort() function sorts associative arrays in descending order by value
+*/
+
 
 
 ?>
